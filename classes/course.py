27f11b6cef9751ab5ee392419e8f6d7f5f0ce57d
kintu hamuzah <kintuhamuzah66@gmail.com>
2026-06-11 13:21:33 +0300

    fixed course

--- classes/course.py
+++ classes/course.py
@@ -1,47 +1,46 @@
 class Course:
     def __init__(self, course_id, course_name):
-        self.course_id = course_id
-        self.course_name = course_name
+        self._course_id = course_id
+        self._course_name = course_name
         
         self.course_units = []
 
+    # course id getter and setter
     @property
     def course_id(self):
         return self._course_id
     
     @course_id.setter
     def course_id(self, id):
-        try:
-            if not id:
-                raise ValueError("Invalid id")
-        except ValueError:
-            print("Course ID cannot be empty")
-        
+        if not id:
+            raise ValueError("Course ID cannot be empty")
+        if " " in id or not id.isalnum():
+            raise ValueError("Course ID can only contain letters and numbers")
+
         self._course_id = id
 
+    # course name getter and setter
     @property
     def course_name(self):
         return self._course_name
     
     @course_name.setter
     def course_name(self, name):
-        try:
-            if not name:
-                raise ValueError("Invalid name")
-        except ValueError:
-            print("Course name cannot be empty")
+        if not name:
+                raise ValueError("Course name cannot be empty")
 
         self._course_name = name
 
+    # course units getter and setter
     @property
     def course_units(self):
         return len(self._course_units), self._course_units 
 
     @course_units.setter
     def course_units(self, course_units):
-        try:
-            if not course_units:
-                raise ValueError("Invalid error")
-        except ValueError:
-            print(f"You added zero course units to {self.course_name}")
-        
+        if not course_units:
+                raise ValueError("Course units cannot be empty")
+        for unit in course_units:
+             if not unit:
+                raise ValueError("Course unit cannot be empty")
+             self._course_units.append(unit)
