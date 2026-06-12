22c69178faa82025a0ae82d864d24069d0efed66
kintu hamuzah <kintuhamuzah66@gmail.com>
2026-06-12 12:57:12 +0300

    fixed lecturer

--- classes/lecturer.py
+++ classes/lecturer.py
@@ -4,19 +4,18 @@ class lecturer(Person):
     def __init__(self, id, first_name, last_name, other_name=None):
         super().__init__(id, first_name, last_name, other_name)
 
-        self._courses = []
+        self._course_units = []
 
     # courses getter and setter
     @property
-    def courses(self):
-        return self._courses
+    def course_units(self):
+        return self._couse_units
     
-    @courses.setter
-    def courses(self, courses):
-        if not courses:
+    @course_units.setter
+    def course_units(self, courseunits):
+        if not courseunits:
             raise ValueError("Courses cannot be empty")
-        for course in courses:
+        for course in courseunits:
             if not course:
                 raise ValueError("Course cannot be empty")
             self._courses.append(course)
-
