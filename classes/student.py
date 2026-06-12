b71bccccadc591cd5d3c231a9c4319bf835b1359
kintu hamuzah <kintuhamuzah66@gmail.com>
2026-06-12 13:06:29 +0300

    fixed student class

--- classes/student.py
+++ classes/student.py
@@ -5,6 +5,8 @@ class Student(Person):
         super().__init__(id, first_name, last_name, other_name)
         self._major = major
 
+        self._course_units = []
+
     # major getter and setter
     @property
     def major(self):
@@ -16,3 +18,16 @@ class Student(Person):
             raise ValueError("Major cannot be empty")
         self._major = value
 
+    # courses getter and setter
+    @property
+    def course_units(self):
+        return self._couse_units
+    
+    @course_units.setter
+    def course_units(self, courseunits):
+        if not courseunits:
+            raise ValueError("Courses cannot be empty")
+        for course in courseunits:
+            if not course:
+                raise ValueError("Course cannot be empty")
+            self._courses.append(course)
