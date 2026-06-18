76ef8fc64da5aa398dd69571dd63cae8e75fa5cb
kintu hamuzah <kintuhamuzah66@gmail.com>
2026-06-18 10:32:07 +0300

    added bit of department

---
+++ classes/department.py
@@ -0,0 +1,22 @@
+import logging
+logger = logging.getLogger(__name__)
+
+class Department:
+    def __init__(self, name):
+        self.name = name
+
+        self.courses = []
+
+    # name properties
+    @property
+    def name(self):
+        return self._name 
+    
+    @name.setter
+    def name(self, value):
+        if not value or str(value).strip():
+            # log and raise error
+            logger.error("Failed to set department name. It cannot be empty")
+            raise ValueError("Department name cannot be empty!")
+        # set department name
+        self._name = value
