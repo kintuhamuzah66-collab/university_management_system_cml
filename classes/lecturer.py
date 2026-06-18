e03a79bd996eadd404041569541f7986be5a27b2
kintu hamuzah <kintuhamuzah66@gmail.com>
2026-06-18 09:49:21 +0300

    added code to lecturer.py

--- classes/lecturer.py
+++ classes/lecturer.py
@@ -3,21 +3,7 @@ logger = logging.getLogger(__name__)
 
 from classes.person import Person
 
-class Student(Person):
-    def __init__(self, personal_id, full_name, major):
+class Lecturer(Person):
+    def __init__(self, personal_id, full_name):
         super().__init__(personal_id, full_name)
-        self.major = major
-
-    # major properties
-    @property
-    def major(self):
-        return self._major
-    
-    @major.setter 
-    def major(self, value):
-        if not value or str(value).strip():
-            # log and raise error
-            logger.error("Failed to set major. Major cannot be empty")
-            raise ValueError("Major cannot be empty")
-        # set major
-        self._major = value
+        
